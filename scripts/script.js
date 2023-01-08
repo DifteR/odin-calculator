cb7878e85ce5d7168b31fa733d018c3823713858
Tilen Marš <its0black0magic@gmail.com>
2023-01-08 08:12:04 +0000

    Loads of changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,31 @@
-const textfield = document.getElementById("textfield");
+/*const textfield = document.getElementById("textfield");
+const numberElements = document.getElementsByClassName("number");
+
+numberElements.addEventListener(click, function (textfield) {
+    textfield = textfield + this.value;
+})*/
+
+document.addEventListener("DOMContentLoaded", function(){
+    var textBox = document.querySelector("#textfield");
+    var numbers = document.querySelectorAll(".number");
+
+    numbers.forEach(num => {
+        num.addEventListener("click", function(e){
+            console.log(this.value)
+            textBox.value += this.value; 
+        })
+    });
+})
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    var textBox = document.querySelector("#textfield");
+    var numbers = document.querySelectorAll(".operator");
+
+    numbers.forEach(num => {
+        num.addEventListener("click", function(e){
+            console.log(this.value)
+            textBox.value += this.value; 
+        })
+    });
+})
