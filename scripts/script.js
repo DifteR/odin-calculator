2777ea6e7095d689282c3ccc1ac51b1968d1af37
Tilen Marš <marstilen@gmail.com>
2023-01-09 08:32:04 +0100

    add equals functionality, clean up code

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,23 @@
 var values = [];
-var operators = [];
 
-document.addEventListener("DOMContentLoaded", function(){
-    var textBox = document.querySelector("#textfield");
-    var numbers = document.querySelectorAll(".number");
+document.addEventListener("DOMContentLoaded", function () {
+  var textBox = document.querySelector("#textfield");
+  var numbers = document.querySelectorAll(".number");
+  var operators = document.querySelectorAll(".operator");
 
-    numbers.forEach(num => {
-        num.addEventListener("click", function(e){
-            if (!isNaN(textBox.value)) {
-                textBox.value += this.value;
-                
-            }
-            else{
-                operators.push(textBox.value);
-                textBox.value = this.value;
-            }
-             
-        })
+  numbers.forEach((num) => {
+    num.addEventListener("click", function (e) {
+      textBox.value += this.value;
     });
-})
+  });
 
-
-document.addEventListener("DOMContentLoaded", function(){
-    var textBox = document.querySelector("#textfield");
-    var numbers = document.querySelectorAll(".operator");
-
-    numbers.forEach(num => {
-        num.addEventListener("click", function(e){
-            values.push(textBox.value);
-            textBox.value = this.value; 
-            console.table(values);
-        })
+  operators.forEach((num) => {
+    num.addEventListener("click", function (e) {
+      values.push(textBox.value);
+      textBox.value += this.value;
     });
-})
+  });
+  document.getElementById("equals").addEventListener("click", function (e) {
+    textBox.value = eval(textBox.value);
+  });
+});
